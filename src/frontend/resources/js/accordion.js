9de51cd0436109b20a978456d5e91c7b2ff56cb7
Roland Schaub <roland.schaub@zephir.ch>
2017-04-04 13:41:09 +0200

    Update to accordion JS

--- src/frontend/resources/js/accordion.js
+++ src/frontend/resources/js/accordion.js
@@ -2,13 +2,24 @@ $(document).ready(function(){
     var url = document.location.href;
     var idx = url.indexOf("#");
     var hash = idx != -1 ? url.substring(idx+1) : "";
+    
+    setTimeout(function(){
+    	targetAccordion(hash)
+    }, 500);
+    
+    $('a[href*=\\#]').click(function(){
+    	targetAccordion(hash)
+    });
+	
+	function targetAccordion(hash) {
+		if (hash != "") {
+	
+			// Check checkbox or radio button that has id equal to url hash
+			$('#' + hash + '.accordion__item .accordion__input').prop("checked", true);
 
-    if (hash != "") {
-        // Check checkbox or radio button that has id equal to url hash
-        $('#' + hash + '.accordion__item .accordion__input').prop("checked", true);
-
-        // After opening accordion, jump to accordion (as position has changed
-        // after opening the accordion in the previous step)
-        // location.href = "#" + hash;
-    }
+			// After opening accordion, jump to accordion (as position has changed
+			// after opening the accordion in the previous step)
+			location.href = "#" + hash;
+		}
+	}
 });
